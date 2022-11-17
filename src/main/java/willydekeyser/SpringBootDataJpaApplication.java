d1ad03fd32ef34c7fda739175b2418e732ca105a
willydekeyser <willy.de.keyser@skynet.be>
2022-11-17 18:57:38 +0100

    Update add Command Line Runner

--- src/main/java/willydekeyser/SpringBootDataJpaApplication.java
+++ src/main/java/willydekeyser/SpringBootDataJpaApplication.java
@@ -1,7 +1,9 @@
 package willydekeyser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
@@ -9,5 +11,11 @@ public class SpringBootDataJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
-
+	
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Run CommandLineRunner!");
+		};
+	}
 }
