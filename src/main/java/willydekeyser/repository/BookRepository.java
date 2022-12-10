99f6005cb65a69462e2c97dab736670d0735647e
willydekeyser <willy.de.keyser@skynet.be>
2022-12-10 11:54:28 +0100

    Update Repository Classes

--- src/main/java/willydekeyser/repository/BookRepository.java
+++ src/main/java/willydekeyser/repository/BookRepository.java
@@ -3,11 +3,9 @@ package willydekeyser.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import willydekeyser.model.Book;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 	List<Book> findAllByMemberId(Long id);
